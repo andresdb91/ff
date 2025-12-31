6f7611ac815997f5ecd34494cce64a2ceb73b14e
Andres Bonilla <andresdb91@gmail.com>
2025-12-31 20:58:36 -0300

    Basic axum server

---
+++ ff/src/domain.rs
@@ -0,0 +1,3 @@
+struct FeatureFlag {
+    state: bool,
+}
---
+++ ff/src/lib.rs
@@ -0,0 +1,11 @@
+mod api;
+mod domain;
+mod app;
+mod adapter;
+mod utils;
+
+pub fn hello() -> String {
+    String::from("Hello World!")
+}
+
+pub fn setup() {}
