6727c9f3586683c8883fcbad117233492132c243
Andres Bonilla <andresdb91@gmail.com>
2026-06-16 16:06:03 -0300

    Minimal flag type state recovery

--- crates/fflags/src/domain.rs
+++ crates/fflags/src/domain.rs
@@ -1,23 +1,33 @@
 // Hold domain models and invariants
 
+use tracing::debug;
+
 #[derive(Debug)]
 pub enum FeatureFlagKind {
-    Release,
-    Experiment,
-    Operations,
-    Access,
+    Basic,
+    // Release,
+    // Experiment,
+    // Operations,
+    // Access,
 }
 
 pub struct FeatureFlag {
-    state: bool,
-    name: String,
+    pub name: String,
     kind: FeatureFlagKind,
+    state: bool,
 }
 
 impl FeatureFlag {
     pub fn new(name: String, kind: Option<FeatureFlagKind>) -> FeatureFlag {
         println!("Feature Flag created: {} - {:?}", name, kind);
-        FeatureFlag { state: false, name: name, kind: kind.unwrap_or(FeatureFlagKind::Release) }
+        FeatureFlag { state: false, name: name, kind: kind.unwrap_or(FeatureFlagKind::Basic) }
+    }
+
+    pub fn get_state(&self) -> bool {
+        debug!("Evaluated flag - {}: {}", self.name, self.state);
+        match self.kind {
+            FeatureFlagKind::Basic => self.state
+        }
     }
 }
 
